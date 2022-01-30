42fc1ba396485f6aa3431bc88170768ea2c312f5
Brynne Eastman <brynne.eastman@gmail.com>
2022-01-30 11:50:34 -0500

    updated resolvers.js

--- power-up/server/schemas/resolvers.js
+++ power-up/server/schemas/resolvers.js
@@ -1,10 +1,58 @@
 
-const { AuthenticationError} = require('apollo-server-express');
+const { AuthenticationError, UserInputError} = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-      
+      // me() method
+      me: async (parent, args, context) => {
+          if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                
+                return userData;
+          }
+
+          throw new AuthenticationError('Not logged in')
+      }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveExercise: async (parent, { input }, context ) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedExercises: input } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in');
+        }
     }
   };
   
