7ce4499a195862b18bbbcec4954ca8b77f6965e0
Bbaker1989 <86622638+Bbaker1989@users.noreply.github.com>
2022-01-31 19:35:11 -0500

    updated front end

--- power-up/client/src/App.js
+++ power-up/client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+import '../src/main.scss';
+
+// Components of the page
+import Nav from '../components/nav';
+import Homepage from '../components/homepage';
+
+//Page Components
+
+//Sign Up
+
+
+//Login In
+
+//Power-Up App Logged in Page
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <Nav></Nav>
+        <Homepage></Homepage>
+      </main>
     </div>
   );
 }
+
+
 
 export default App;
