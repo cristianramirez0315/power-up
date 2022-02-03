f3d1730f4b677ed38c8776f7819aeb9ee4ef1e04
Brynne Eastman <brynne.eastman@gmail.com>
2022-02-02 21:16:12 -0500

    trying to fix login/signup

--- power-up/server/schemas/resolvers.js
+++ power-up/server/schemas/resolvers.js
@@ -1,5 +1,5 @@
-
-const { AuthenticationError, UserInputError} = require('apollo-server-express');
+const { User } = require('../models')
+const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -23,6 +23,13 @@ const resolvers = {
             const token = signToken(user);
 
             return { token, user };
+        },
+        updateUser: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+            }
+
+            throw new AuthenticationError('Nog logged in');
         },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
