1227420103680432ce95e39f27694d4ec4267ddf
Lissette <lisssettemvega@gmail.com>
2022-02-02 21:04:58 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/power-up', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/powerup', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   //useCreateIndex: true,
